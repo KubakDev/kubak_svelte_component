06e9120fe7544084a6a5d1cee8e2a338e7aebc02
bnar98 <bnar.paskandy@gmail.com>
2023-04-12 14:33:03 +0300

    create headers component

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,5 @@
 import MainCard from "./components/cards/MainCard.svelte";
 import HorizontalCard from "./components/cards/HorizontalCard.svelte";
-export { MainCard, HorizontalCard };
+import DefaultNavigationHeader from "./components/headers/defaultNavigationHeader.svelte";
+import DefaultSectionHeader from "./components/headers/defaultSectionHeader.svelte";
+export { MainCard, HorizontalCard, DefaultNavigationHeader, DefaultSectionHeader };
