5fb4b10b5951caeab8115a83acef6c2599846e10
Arman Hadi <arman.kubak@gmail.com>
2023-05-13 15:40:38 +0300

    update

--- dist/pages/newsDetail.svelte
+++ dist/pages/newsDetail.svelte
@@ -8,13 +8,12 @@ console.log("bnar bnar bnar be jaw ");
 </script>
 
 <div class="w-full min-h-screen bg-[#292e36] flex flex-col items-center text-white">
-	<h1> Welcome bnar</h1>
-	<!-- <div class="">
+	<div class="">
 		{#if data.images.length > 1}
 			<Carousel images={data.images} showCaptions={false} divClass="bg-red-400" />
 		{/if}
 	</div>
 	<div>
 		{@html data.long_description}
-	</div> -->
+	</div>
 </div>
