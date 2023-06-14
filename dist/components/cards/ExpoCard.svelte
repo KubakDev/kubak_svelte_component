c96d3e8d958f029b88b6949fa849e53b2add325b
Arman Hadi <arman.kubak@gmail.com>
2023-06-14 16:45:52 +0300

    update

--- dist/components/cards/ExpoCard.svelte
+++ dist/components/cards/ExpoCard.svelte
@@ -15,7 +15,7 @@ export let primaryColor = undefined;
 export let secondaryColor = undefined;
 export let backgroundColor = undefined;
 export let overlayBackgroundColor = undefined;
-$: cardClass = classNames(' w-full cursor-pointer   shadow-xl rounded-3xl  relative ', cardType === CardType.Main ? ' max-w-[400px] min-w-[300px]' : ' max-w-[600px]');
+$: cardClass = classNames(' w-full cursor-pointer   shadow-xl rounded-3xl  relative ', primaryColor ? ' ' : ' bg-primary', cardType === CardType.Main ? ' max-w-[400px] min-w-[300px]' : ' max-w-[600px]');
 </script>
 
 <!-- {#if cardType === CardType.Main} -->
